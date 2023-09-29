eedd480b56b52624f53063ac9c9ef279309b7b48
William Jeffreys <wjeffreys96@gmail.com>
2023-09-29 14:19:18 -0700

    housecleaning

--- roseware/celery.py
+++ roseware/celery.py
@@ -1,9 +1,6 @@
 import os, datetime
-from django.core import management
 from celery import Celery
 from celery.schedules import crontab
-
-
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roseware.settings.base")
@@ -12,12 +9,6 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-# # Celery hijacks the root logger and configures it itself by default. 
-# # This prevents Celery from doing that. 
-# @setup_logging.connect
-# def celery_setup_logging(*args, **kwargs):
-#     pass
 
 # Schedule tasks
 app.conf.beat_schedule = {
