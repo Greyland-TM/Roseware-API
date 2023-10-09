593bd98199b1185f8214ab05b35f95958f3bc045
William Jeffreys <wjeffreys96@gmail.com>
2023-10-09 15:08:32 -0700

    remove knox urls

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -9,8 +9,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", include("knox.urls")),
-    path("login", LoginAPIView.as_view()),
+    path("login/", LoginAPIView.as_view()),
     path('logout', LogoutView.as_view(), name='logout'),
     path('customer/', CustomerAPIView.as_view(), name='customer'),
     path('create-customer/', CreateCustomerAPIView.as_view(), name='create-customer'),
